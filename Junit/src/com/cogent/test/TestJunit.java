42e925d2c2dc4636101fc3b5a78873810e588dd2
DanielGeb <dfgebreh@gmail.com>
2021-02-21 15:42:09 -0500

    added a print

--- Junit/src/com/cogent/test/TestJunit.java
+++ Junit/src/com/cogent/test/TestJunit.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestJunit {
 	   @Test
 	   public void testSetup() {
-	      String str= "I am done with Junit setup";
-	      assertEquals("I am done with Junit setup",str);
+	      String str= "Junit setup is done";
+	      assertEquals("Junit setup is done",str);
 	   }
 	}
