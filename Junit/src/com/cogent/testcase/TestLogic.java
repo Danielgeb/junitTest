fedd60f00f7bdbaae70984cf27cbee28f89cfd8c
DanielGeb <dfgebreh@gmail.com>
2021-02-21 15:20:43 -0500

    junit test

---
+++ Junit/src/com/cogent/testcase/TestLogic.java
@@ -0,0 +1,17 @@
+package com.cogent.testcase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.cogent.logic.*;  
+ 
+  
+public class TestLogic {  
+  
+    @Test  
+    public void testFindMax(){  
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+    }  
+}
